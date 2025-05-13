b3c27a40d8321f8c81cf5f2dc737b32038522cf4
shubham sharma <shubhamtech004@gmail.com>
2025-05-13 23:57:56 +0530

    project setup

---
+++ src/pages/index.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export default function Home() {
+  return (
+        <div className="text-emerald-300 font-bold italic text-center mt-8"> 
+           Biobranch cooming soon
+        </div>
+  );
+}
