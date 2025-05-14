40e48a26cf7046980865e5b85650bce4768d9ace
shubham sharma <shubhamtech004@gmail.com>
2025-05-14 20:02:34 +0530

    navbar footer setup

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,11 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export default function Home() {
   return (
-        <div className="text-emerald-300 font-bold italic text-center mt-8"> 
-           Biobranch cooming soon
+        <div className="">
+         home
         </div>
   );
 }
