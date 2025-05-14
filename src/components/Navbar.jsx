8e75e755ae76db6dd25eaca88dee7092251569fd
shubham sharma <shubhamtech004@gmail.com>
2025-05-14 23:46:34 +0530

    navbar section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,12 +5,16 @@ const Navbar = () => {
   return (
     <div className='p-4 bg-amber-400 items-center text-white font-bold flex justify-around'>
       <div>
-        Biobranch
+         <Link href="/">Biobranch</Link>
       </div>
-      <div className="flex gap-4">
+      <div className="flex gap-6">
         <Link href="/about">About</Link>
         <Link href="/contact">Contact</Link>
         <Link href="/price">Price</Link>
+      </div>
+      <div className="flex gap-3">
+         <Link href="/login">Login</Link>
+         <Link href="/register">Register</Link>
       </div>
     </div>
   )
