2f4cf05e23dd50a1c89fe59c62f8e7a2eb8ab9be
shubham sharma <shubhamtech004@gmail.com>
2025-05-18 02:18:22 +0530

    db connection done

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
+import connectDB from "@/libs/MongoClient";
+
+export default function GET(req, res) {
+  connectDB()
   res.status(200).json({ name: "John Doe" });
 }
