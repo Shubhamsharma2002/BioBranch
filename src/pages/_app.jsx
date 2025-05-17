e15e05490b4de60bde95cea12593b38c8f1d0e65
shubham sharma <shubhamtech004@gmail.com>
2025-05-17 23:19:51 +0530

    layout setup

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,6 +5,12 @@ import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
-     <Component {...pageProps} />
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <main className="flex-1">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,12 +6,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Navbar />
-      <main className="flex-1 ">
-        <IntroSection/>
-      </main>
-      <Footer />
-    </div>
+    
+    <IntroSection/>
   );
 }
