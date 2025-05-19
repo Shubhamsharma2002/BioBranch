3b609f99898956a29b98a7cbac565a31fc7b2e61
shubham sharma <shubhamtech004@gmail.com>
2025-05-19 23:36:55 +0530

    footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer=()=> {
   return (
-    <div className='bg-green-800 p-5 text-white font-bold text-center' >All right reserved to shubham sharma</div>
+    <div className='bg-green-800 p-5 text-white font-bold text-center' >All right reserved to shubham sharma Made in India</div>
   )
 }
 
