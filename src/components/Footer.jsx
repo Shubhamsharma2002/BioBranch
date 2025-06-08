fabfc71307243880fd8740b44091eedae0bbfad7
shubham sharma <shubhamtech004@gmail.com>
2025-06-09 04:52:23 +0530

    folder setup

--- src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Footer=()=> {
-  return (
-    <div className='bg-green-800 p-5 text-white font-bold text-center' >All right reserved to shubham sharma Made in India</div>
-  )
-}
-
-export default Footer
