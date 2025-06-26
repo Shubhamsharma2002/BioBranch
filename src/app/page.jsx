59b6c2e63f584fa393f26b980b6f592459e295d9
shubham sharma <shubhamtech004@gmail.com>
2025-06-26 20:07:28 +0530

    intro section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,8 +5,16 @@ export default function Home() {
   return (
       <main>
         <Header/>
-        <section>
-          
+        <section className="p-6 pt-32 max-w-4xl mx-auto">
+           <div className="max-w-md mb-8">
+                  <h1 className="text-6xl font-bold">Your one Link <br/> for everything</h1>
+                  <h2 className="text-gray-500 text-xl mt-6">Share your links, social profiles, contact info and more on one page</h2>
+           </div>
+           <form className="inline-flex items-center shadow-lg shadow-gray-500/20">
+            <span className="bg-white py-4 pl-4">biobranch.to/</span>
+            <input type="text" className="py-4"  placeholder="username"/>
+            <button type="submit" className="bg-blue-500 text-white py-4 px-6">Join for free</button>
+           </form>
         </section>
       </main>
   );
