77e6bbf1b2b5c439e9a2659abb1e36020aa879f7
shubham sharma <shubhamtech004@gmail.com>
2025-09-03 23:26:40 +0530

    test

--- src/components/buttons/LoginWithGoogle.jsx
+++ src/components/buttons/LoginWithGoogle.jsx
@@ -2,6 +2,8 @@ import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function LoginWithGoogle() {
   return (
+
+    
     <div className="w-full mt-3">
       <button className="w-full py-3 px-6 flex items-center justify-center gap-3 rounded-lg shadow-sm  bg-green-400 text-white cursor-pointer">
         <FontAwesomeIcon icon={faGoogle} className="h-5 w-5 text-orange-600" />
