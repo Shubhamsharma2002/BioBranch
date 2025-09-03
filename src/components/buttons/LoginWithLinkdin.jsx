97674c5c16e410ce9f790edf8c4f65e43f423720
shubham sharma <shubhamtech004@gmail.com>
2025-09-03 23:26:27 +0530

    test

--- src/components/buttons/LoginWithLinkdin.jsx
+++ src/components/buttons/LoginWithLinkdin.jsx
@@ -2,6 +2,8 @@ import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 export default function LoginWithLinkdin() {
   return (
+
+    
     <div className="w-full mt-3">
       <button className="w-full py-3 px-6 flex items-center justify-center gap-3 rounded-lg shadow-sm  bg-blue-500 text-white cursor-pointer">
         <FontAwesomeIcon icon={faLinkedinIn} className="h-5 w-5" />
