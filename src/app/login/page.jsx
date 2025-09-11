87c79deba02d4a1c6a4e84a7731ef8f8bcf37ae2
shubham sharma <shubhamtech004@gmail.com>
2025-09-11 23:37:07 +0530

    test

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,9 +5,7 @@ export default function Login() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 ">
       {/* Left: Login Form */}
-
-      
-      <div className="p-4 max-w-xs mx-auto flex flex-col justify-center">
+        <div className="p-4 max-w-xs mx-auto flex flex-col justify-center">
         <h1 className="text-4xl font-bold text-center mb-4">Sign In</h1>
         <LoginWithGoogle />
         <LoginWithLinkdin/>
