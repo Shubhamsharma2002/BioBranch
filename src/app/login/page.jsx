64ead5584ee2f4deb34e9bdc09c6a8825acb37bd
shubham sharma <shubhamtech004@gmail.com>
2025-09-11 23:37:24 +0530

    test

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -5,20 +5,17 @@ export default function Login() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 ">
       {/* Left: Login Form */}
-        <div className="p-4 max-w-xs mx-auto flex flex-col justify-center">
+      <div className="p-4 max-w-xs mx-auto flex flex-col justify-center">
         <h1 className="text-4xl font-bold text-center mb-4">Sign In</h1>
         <LoginWithGoogle />
-        <LoginWithLinkdin/>
+        <LoginWithLinkdin />
       </div>
-
-
 
       {/* Right: Image or anything else */}
       <div className="flex items-center justify-center bg-gray-100">
         {/* Add your image or content here */}
         {/* <img src="/login-image.png" alt="Login Illustration" className="w-3/4" /> */}
       </div>
-      
     </div>
   );
 }
