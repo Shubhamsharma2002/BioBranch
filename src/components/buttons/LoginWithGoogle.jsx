6867dc0050d11744e537f448c4cd4e26a7f94cf3
shubham sharma <shubhamtech004@gmail.com>
2025-09-12 22:55:27 +0530

    test

--- src/components/buttons/LoginWithGoogle.jsx
+++ src/components/buttons/LoginWithGoogle.jsx
@@ -7,7 +7,7 @@ export default function LoginWithGoogle() {
     <div className="w-full mt-3">
       <button className="w-full py-3 px-6 flex items-center justify-center gap-3 rounded-lg shadow-sm  bg-green-400 text-white cursor-pointer">
         <FontAwesomeIcon icon={faGoogle} className="h-5 w-5 text-orange-600" />
-        <span className="text-base font-medium">Sign in with Google....</span>
+        <span className="text-base font-medium">Sign in with Google... .</span>
       </button>
     </div>
   );
