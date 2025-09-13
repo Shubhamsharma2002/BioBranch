8ca74b8dc40d19bdfbbc624b060a963e9c7ca43f
shubham sharma <shubhamtech004@gmail.com>
2025-09-13 23:28:10 +0530

    test

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer
-        ....         all right reserved
+    <div>Footer all right reserved
     </div>
   )
 }
