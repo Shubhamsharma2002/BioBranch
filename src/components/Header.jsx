cf0892573eb3158f5e1670cf9e9af9cc8d4901b4
Shubham Sharma <shubhamtech004@gmail.com>
2026-06-18 12:00:00 +0530

    Commit for 18 June

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,4 @@
 import Link from "next/link";
-
-
-
 export default function Header() {
   return (
     <nav className="flex flex-col md:flex-row justify-between items-center p-4 bg-white shadow-md">
